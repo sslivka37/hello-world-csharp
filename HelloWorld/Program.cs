15da4351d82e18711d93042eeb207f1993a14ce5
sslivka37 <sslivka37@gmail.com>
2019-05-19 23:03:40 -0400

    Sarah first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my first C# project using Visual Studio!");
+        }
+    }
+}
